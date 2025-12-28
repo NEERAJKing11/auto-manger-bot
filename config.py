92d28430270f7c714d9a931e2a0bad285a854b2a
NEERAJKing11 <neerajking3605@gmail.com>
2025-12-28 20:21:02 +0530

    Update bot token and database file configuration

--- config.py
+++ config.py
@@ -1,14 +1,6 @@
 # --- CONFIGURATION ---
+BOT_TOKEN = "8394910121:AAHIFz-bYQpYUELu_Z4WnRpqlnjPjqnmNmk"  # Apna Token Dalein
+OWNER_ID = 6761345074             # Apni ID (Number only)
 
-# Apna Bot Token yahan dalein
-BOT_TOKEN = "8159407862:AAEmxgr8F28L7ndZdkjdUD6WjyvEOTqd5Nk"
-
-# Owner Details (Jo aapne di hain)
-OWNER_ID = 6761345074
-OWNER_USERNAME = "RoyalKing_7X4"
-
-# Bot Start Photo (Exam/Study Theme)
-START_IMG = "https://cdn-icons-png.flaticon.com/512/3408/3408591.png"
-
-# File name for database
-DB_FILE = "ultra_bot_data.json"
+# Database File Name
+DB_FILE = "pro_database.json"
