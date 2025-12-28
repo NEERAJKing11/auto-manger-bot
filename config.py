2471b4bfc5ebef54ffa87c2fa8dd25899db79772
NEERAJKing11 <neerajking3605@gmail.com>
2025-12-28 22:15:36 +0530

    Update bot configuration values

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
 # --- CONFIGURATION ---
-
-# Token (BotFather se lein)
-BOT_TOKEN = "7893039422:AAF7231DzMwZShg4S-wEz5yYOOYAv0UmfXE"
-
-# Owner ID (Sirf Number)
-OWNER_ID = 6761345074
-
-# Database File
-DB_FILE = "ultra_database.json"
-
-# Bot Start Photo (Coaching Banner/Logo)
+BOT_TOKEN = "8558108219:AAF6ZhXo8MF5xSmgfTaU25lNHKCYJIWF6eM" 
+OWNER_ID = 6761345074  # Sirf Number
+DB_FILE = "final_database.json"
 START_IMG = "https://cdn-icons-png.flaticon.com/512/3408/3408591.png"
