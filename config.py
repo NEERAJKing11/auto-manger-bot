f5f0508dba682b2ef3fdddd07802b928acf5cd01
NEERAJKing11 <neerajking3605@gmail.com>
2025-12-28 20:57:12 +0530

    Modify bot configuration settings
    Updated bot token and database file name, added start image URL.

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 # --- CONFIGURATION ---
-BOT_TOKEN = "8394910121:AAHIFz-bYQpYUELu_Z4WnRpqlnjPjqnmNmk"  # Apna Token Dalein
-OWNER_ID = 6761345074             # Apni ID (Number only)
 
-# Database File Name
-DB_FILE = "pro_database.json"
+# Token (BotFather se lein)
+BOT_TOKEN = "7893039422:AAF7231DzMwZShg4S-wEz5yYOOYAv0UmfXE"
+
+# Owner ID (Sirf Number)
+OWNER_ID = 6761345074
+
+# Database File
+DB_FILE = "ultra_database.json"
+
+# Bot Start Photo (Coaching Banner/Logo)
+START_IMG = "https://cdn-icons-png.flaticon.com/512/3408/3408591.png"
